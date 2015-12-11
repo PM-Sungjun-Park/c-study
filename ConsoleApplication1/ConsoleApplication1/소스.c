a7e2b6fc6db1814f72c1b886dbaa2bf10ae0304f
PM-Sungjun-Park <khsj15@naver.com>
2015-12-11 20:30:34 +0900

    분기 정리

--- ConsoleApplication1/ConsoleApplication1/소스.c
+++ ConsoleApplication1/ConsoleApplication1/소스.c
@@ -1,10 +1,44 @@
+
+/*
+
+Day 1
+
 #include <stdio.h>
+
 int main(void)
 {
-	int num;
-	num = 20;
+int val_1; // val 1, 2 는 정수값
+int val_2;
+val_1 = 20;
+val_2 = 20; // 값 지정
 
-	printf("%d Hell Chosen \n",num);
-	return 0;
-	
+//선연산과 후연산의 차이
+
+printf(" 기존 값은 모두 20이며 val1은 후연산, val2는 선 연산이다. \n\n");
+
+printf(" val_1 = %d \n", val_1++);
+printf(" val_2 = %d \n\n", ++val_2);
+
+// 연산이후
+printf(" 연산이후의 값입니다 \n\n");
+
+printf(" val_1 = %d \n", val_1);
+printf(" val_2 = %d \n\n\n", val_2);
+
+return 0;
+
+}
+
+end Day1
+
+*/
+
+
+/* Day 2 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int val;  // val은 정수 변수 
 }
